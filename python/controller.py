7022e4077d780499df3919d5eeaea970bd03fc6d
Grant Moe <grant@grantmoe.com>
2020-07-12 10:23:52 -0700

    Streamline code and project structure
    There was no need for two separate client files, so they were merged.
    
    The change also obviated the need for the client program to import
    libevdev.

--- python/controller.py
+++ python/controller.py
@@ -1,48 +1,44 @@
-
 #!/usr/bin/env python3
 import fcntl
 import os
 import sys
 import libevdev
+from libevdev import *
 
-
-class XBox:
+ev_map = {
+    'xbox': {
+        'axis': {
+            'left_stick_horz': EV_ABS.ABS_X,
+            'left_stick_vert': EV_ABS.ABS_Y,
+            'left_trigger': EV_ABS.ABS_Z,
+            'right_stick_horz': EV_ABS.ABS_RX,
+            'right_stick_vert': EV_ABS.ABS_RY,
+            'right_trigger': EV_ABS.ABS_RZ,
+            'dpad_horz': EV_ABS.ABS_HAT0X,
+            'dpad_vert': EV_ABS.ABS_HAT0Y,
+        },
+        'button': {
+            'a_button': EV_KEY.BTN_SOUTH,
+            'b_button': EV_KEY.BTN_EAST,
+            'y_button': EV_KEY.BTN_NORTH,
+            'x_button': EV_KEY.BTN_WEST,
+            'left_shoulder': EV_KEY.BTN_TL,
+            'right_shoulder': EV_KEY.BTN_TR,
+            'select_button': EV_KEY.BTN_SELECT,
+            'start_button': EV_KEY.BTN_START,
+            'mode_button': EV_KEY.BTN_MODE,
+            'left_thumb': EV_KEY.BTN_THUMBL,
+            'right_thumb': EV_KEY.BTN_THUMBR,
+        }
+    }
+}
+class Controller:
     
-    axes = {
-        'ABS_X': 0, 
-        'ABS_Y': 0, 
-        'ABS_Z': 0, 
-        'ABS_RX': 0, 
-        'ABS_RY': 0, 
-        'ABS_RZ': 0, 
-        'ABS_HAT0X': 0, 
-        'ABS_HAT0Y': 0,
-    }
-
-    buttons = {
-        'BTN_SOUTH': 0,
-        'BTN_EAST': 0,
-        'BTN_NORTH': 0,
-        'BTN_WEST': 0,
-        'BTN_TL': 0,
-        'BTN_TR': 0,
-        'BTN_SELECT': 0,
-        'BTN_START': 0,
-        'BTN_MODE': 0,
-        'BTN_THUMBL': 0,
-        'BTN_THUMBR': 0,
-    }
-
-    axis_info = {}
-
-    def __init__(self, path='/dev/input/by-id/usb-Microsoft_Controller_3039363431313739383635393433-event-joystick'):
+    def __init__(self, ctr_type='xbox', path='/dev/input/by-id/usb-Microsoft_Controller_3039363431313739383635393433-event-joystick'):
         try:
             self.fd_ = open(path, 'rb')
             fcntl.fcntl(self.fd_, fcntl.F_SETFL, os.O_NONBLOCK)
-            self.dev_ = libevdev.Device(self.fd_)
-            for e in self.dev_.evbits[libevdev.EV_ABS]:
-                self.axis_info[e.name] = self.dev_.absinfo[e]
-
+            self.dev_ = Device(self.fd_)
         except IOError as e:
             import errno
             if e.errno == errno.EACCES:
@@ -52,56 +48,50 @@ class XBox:
             else:
                 raise e
 
-    def update_deadzone(self, val):
-        ai = libevdev.InputAbsInfo(flat=val)
-        self.dev_.absinfo[libevdev.EV_ABS.ABS_X] = ai
+        self.map_ = ev_map[ctr_type]
+        self.dev_.absinfo[EV_ABS.ABS_X] = InputAbsInfo(flat=3000)
 
 
-    def poll(self):
+    def steering_deadzone(self, val):
+        if val is None:
+            return self.dev_.absinfo[EV_ABS.ABS_X].flat
+        else:
+            ai = InputAbsInfo(flat=val)
+            self.dev_.absinfo[EV_ABS.ABS_X] = ai
+
+    # see if this counts as enough work to actually refresh
+    def update(self):
         try:
             for event in self.dev_.events():
-                if event.matches(libevdev.EV_ABS):
-                    self.axes[event.code.name] = event.value
-                elif event.matches(libevdev.EV_KEY):
-                    self.buttons[event.code.name] = event.value
-        except libevdev.EventsDroppedException:
-            print('synching')
+                if event.matches(EV_ABS):
+                    continue
+                elif event.matches(EV_KEY):
+                    continue
+        except EventsDroppedException:
             for sync_event in self.dev_.sync():
-                if sync_event.matches(libevdev.EV_ABS):
-                    self.axes[sync_event.code.name] = sync_event.value
-                if sync_event.matches(libevdev.EV_KEY):
-                    self.buttons[sync_event.code.name] = sync_event.value
+                if sync_event.matches(EV_ABS):
+                    continue
+                if sync_event.matches(EV_KEY):
+                    continue
         except:
             raise Exception('polling failure')
 
     # to get x' in [a, b],
     # x'  = (b - a) * ( (x - x_min) / (x_max - x_min) ) + a
-    def steer(self, low=-1, high=1):
-        x_val = self.axes['ABS_X']
-        x_flat = self.dev_.absinfo[libevdev.EV_ABS.ABS_X].flat
-        x_min = self.dev_.absinfo[libevdev.EV_ABS.ABS_X].minimum
-        x_max = self.dev_.absinfo[libevdev.EV_ABS.ABS_X].maximum
+    def norm(self, ax, low=-1.0, high=1.0):
+        ev = self.map_['axis'][ax]
+        v_val = self.dev_.value[ev]
+        v_min = self.dev_.absinfo[ev].minimum
+        v_max = self.dev_.absinfo[ev].maximum
+        return (high - low) * (v_val - v_min) / (v_max - v_min) + low
 
-        return (high - low) * (x_val - x_min) / (x_max - x_min) + low
+    def button(self, btn):
+        return self.dev_.value[self.map_['button'][btn]]
 
-    def throttle(self, low=-1, high=1):
-        f_val = self.axes['ABS_RZ']
-        f_min = self.dev_.absinfo[libevdev.EV_ABS.ABS_RZ].minimum
-        f_max = self.dev_.absinfo[libevdev.EV_ABS.ABS_RZ].maximum
-
-        r_val = self.axes['ABS_Z']
-        r_min = self.dev_.absinfo[libevdev.EV_ABS.ABS_Z].minimum
-        r_max = self.dev_.absinfo[libevdev.EV_ABS.ABS_Z].maximum
-
-        f = (high - 0) * (f_val - f_min) / (f_max - f_min) + low
-        r = (0 - low) * (r_val - r_min) / (r_max - r_min) + low
-
-        return f - r
 
 
 def main():
-    xbox = XBox()
-
+    ctr = Controller()
 
 if __name__ == "__main__":
     main()
---
+++ python/garnt_client.py
@@ -0,0 +1,158 @@
+import os
+import random
+import json
+import time
+from io import BytesIO
+import base64
+
+from PIL import Image
+import numpy as np
+from gym_donkeycar.core.sim_client import SDClient
+
+from controller import Controller
+
+###########################################
+
+class SimpleClient(SDClient):
+
+    def __init__(self, address, poll_socket_sleep_time=0.01):
+        super().__init__(*address, poll_socket_sleep_time=poll_socket_sleep_time)
+        self.last_image = None
+        self.car_loaded = False
+        self.ctr = Controller()
+        self.dir = f'{os.getcwd()}/../data/{time.strftime("%m_%d_%Y/%H_%M_%S")}'
+        self.data_dir = f'{self.dir}/data'
+        self.img_dir = f'{self.dir}/images'
+        os.makedirs(self.data_dir)
+        os.makedirs(self.img_dir)
+        self.record_count = 0
+
+
+    def on_msg_recv(self, json_packet):
+
+        if json_packet['msg_type'] == "car_loaded":
+            self.car_loaded = True
+
+        if json_packet['msg_type'] == "telemetry":
+            del json_packet['msg_type']                   
+            if json_packet['speed'] > 0.1:
+                imgString = json_packet["image"]
+                image = Image.open(BytesIO(base64.b64decode(imgString)))
+                image.save(f'{self.img_dir}/frame_{self.record_count:04d}.jpeg')
+                with open(f'{self.data_dir}/data_{self.record_count:04d}', 'w') as outfile:
+                    json.dump(json_packet, outfile)
+                    self.record_count += 1 
+
+        #don't have to, but to clean up the print, delete the image string.
+        del json_packet["image"]
+
+        print("got:", json_packet)
+
+
+    def send_controls(self, steering, throttle):
+        p = { "msg_type" : "control",
+                "steering" : steering.__str__(),
+                "throttle" : throttle.__str__(),
+                "brake" : "0.0" }
+        msg = json.dumps(p)
+        self.send(msg)
+
+        #this sleep lets the SDClient thread poll our message and send it out.
+        time.sleep(self.poll_socket_sleep_sec)
+
+    def update(self):
+        # get normed inputs
+        self.ctr.update()
+        st = self.ctr.norm('left_stick_horz', -1.0, 1.0)
+        fw = self.ctr.norm('right_trigger', 0.0, 1.0)
+        rv = self.ctr.norm('left_trigger', 0.0, -1.0)
+        if abs(st) < 0.07:
+            st = 0.0
+        self.send_controls(st, fw + rv)
+
+
+
+###########################################
+## Create client and connect it with the simulator
+
+def drive():
+    # test params
+    host = "127.0.0.1" # "trainmydonkey.com" for virtual racing server
+    port = 9091
+
+    # Start Client
+    client = SimpleClient(address=(host, port))
+    time.sleep(1)
+
+    # Load Scene message
+    #  Choices: 
+    #           'generated_road'
+    #           'generated_track'
+    #           'mountain_track'
+    #           'sparkfun_avc'
+    #           'warehouse'
+    msg = '{ "msg_type" : "load_scene", "scene_name" : "generated_track" }'
+    client.send(msg)
+
+    # Wait briefly for the scene to load.
+    loaded = False
+    while(not loaded):
+        time.sleep(1.0)
+        loaded = client.car_loaded           
+        
+    # Car config
+    msg = '{ "msg_type" : "car_config", "body_style" : "car01", "body_r" : "234", "body_g" : "21", "body_b" : "144", "car_name" : "Grant", "font_size" : "50" }'
+    client.send(msg)
+    time.sleep(1)
+
+    # Camera config
+    # set any field to Zero to get the default camera setting.
+    # this will position the camera right above the car, with max fisheye and wide fov
+    # this also changes the img output to 255x255x1 ( actually 255x255x3 just all three channels have same value)
+    # the offset_x moves camera left/right
+    # the offset_y moves camera up/down
+    # the offset_z moves camera forward/back
+    # with fish_eye_x/y == 0.0 then you get no distortion
+    # img_enc can be one of JPG|PNG|TGA
+    msg = '{ "msg_type" : "cam_config", "fov" : "150", "fish_eye_x" : "1.0", "fish_eye_y" : "1.0", "img_w" : "255", "img_h" : "255", "img_d" : "1", "img_enc" : "PNG", "offset_x" : "0.0", "offset_y" : "3.0", "offset_z" : "0.0", "rot_x" : "90.0" }'
+    client.send(msg)
+    time.sleep(1)
+
+
+    # Controller setup
+    # events = [EV_ABS.ABS_X, EV_ABS.ABS_RZ, EV_ABS.ABS_Z] 
+
+    # adjust steering dead zone. clunky
+    # ctr.dev.absinfo[EV_ABS.ABS_X] = InputAbsInfo(flat=3000)
+
+    # Send random driving controls
+    do_drive = True
+    while do_drive:
+        try:
+            client.update()
+            if client.aborted:
+                print("Client socket problem, stopping driving.")
+                do_drive = False
+        except KeyboardInterrupt:
+            do_drive = False
+
+
+    time.sleep(1.0)
+
+    # Exit Scene
+    msg = '{ "msg_type" : "exit_scene" }'
+    client.send(msg)
+
+    time.sleep(1.0)
+
+    # Close down clients
+    print("waiting for msg loop to stop")
+    client.stop()
+
+    print("client stopped")
+
+
+
+if __name__ == "__main__":
+    drive()
+
