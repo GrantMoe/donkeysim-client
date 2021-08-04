c8e91abb5ec798593fa71d6d0cca4e0d02cd6efe
Grant Moe <grant@grantmoe.com>
2021-08-03 20:58:50 -0700

    Add argument parsing and tweak all things
    Borrowed the "official" test client's argument
    parsing minus LIDAR and the all-env-test option
    
    Corrected telemetry processing error. Moved
    image deletion command to inside the if block where
    it should have been

--- python/garnt_client.py
+++ python/garnt_client.py
@@ -1,16 +1,13 @@
+import argparse
+import uuid
 import os
 import json
 import time
 from io import BytesIO
 import base64
-
 from PIL import Image
-import numpy as np
 from gym_donkeycar.core.sim_client import SDClient
-
 from controller import Controller
-
-###########################################
 
 class SimpleClient(SDClient):
 
@@ -26,29 +23,21 @@ class SimpleClient(SDClient):
         os.makedirs(self.img_dir)
         self.record_count = 0
 
-
     def on_msg_recv(self, json_packet):
-
         if json_packet['msg_type'] == "car_loaded":
             self.car_loaded = True
-
         if json_packet['msg_type'] == "telemetry":
-            del json_packet['msg_type']                   
-            if json_packet['speed'] > 0.1:
+            if json_packet['throttle'] > 0.0:
                 imgString = json_packet['image']
+                del json_packet['image']                  
                 image = Image.open(BytesIO(base64.b64decode(imgString)))
-                image.save(f'{self.img_dir}/frame_{self.record_count:04d}.jpg')
-                # image.save(f'{self.img_dir}/frame_{self.record_count:04d}.png')
-
+                image.save(f'{self.img_dir}/frame_{self.record_count:04d}.png')
                 with open(f'{self.data_dir}/data_{self.record_count:04d}', 'w') as outfile:
                     json.dump(json_packet, outfile)
                     self.record_count += 1 
 
-            # don't have to, but to clean up the print, delete the image string.
-            del json_packet['image']
-
-        print("got:", json_packet)
-
+        if json_packet['msg_type'] != "telemetry":     
+            print("got:", json_packet)
 
     def send_controls(self, steering, throttle):
         p = { "msg_type" : "control",
@@ -57,7 +46,6 @@ class SimpleClient(SDClient):
                 "brake" : "0.0" }
         msg = json.dumps(p)
         self.send(msg)
-
         #this sleep lets the SDClient thread poll our message and send it out.
         time.sleep(self.poll_socket_sleep_sec)
 
@@ -72,62 +60,44 @@ class SimpleClient(SDClient):
         self.send_controls(st, fw + rv)
 
 
-
-###########################################
-## Create client and connect it with the simulator
-
-def drive():
-    # test params
-    host = "127.0.0.1" # "trainmydonkey.com" for virtual racing server
-    port = 9091
-
-    # Start Client
+# Create client and connect it with the simulator
+def run_client(env_name, conf):
+    host = conf["host"] # "trainmydonkey.com" for virtual racing
+    port = conf["port"]
     client = SimpleClient(address=(host, port))
+
     time.sleep(1)
 
-    # Load Scene message
-    #  Choices: 
-    #           'generated_road'
-    #           'generated_track'
-    #           'mountain_track'
-    #           'sparkfun_avc'
-    #           'warehouse'
-    msg = '{ "msg_type" : "load_scene", "scene_name" : "donkey-circuit-launch-track-v0" }'
-    client.send(msg)
+    # # test
+    # msg = '{ "msg_type" : "get_scene_names" }'
+    # client.send(msg)
+    # time.sleep(1) 
 
-    # Wait briefly for the scene to load.
+
+    msg = f'{{ "msg_type" : "load_scene", "scene_name" : "{env_name}" }}'
+    client.send(msg)
     loaded = False
     while(not loaded):
         time.sleep(1.0)
         loaded = client.car_loaded           
         
+
+
     # Car config
-    # msg = '{ "msg_type" : "car_config", "body_style" : "bare", "body_r" : "255", "body_g" : "255", "body_b" : "255", "car_name" : "Grant", "font_size" : "50" }'
-    msg = '{ "msg_type" : "car_config", "body_style" : "f1", "body_r" : "234", "body_g" : "21", "body_b" : "144", "car_name" : "Grant", "font_size" : "50" }'
+    msg = f'{{ "msg_type" : "car_config", "body_style" : "{conf["body_style"]}", "body_r" : "{conf["body_rgb"][0]}", "body_g" : "{conf["body_rgb"][1]}", "body_b" : "{conf["body_rgb"][2]}", "car_name" : "{conf["car_name"]}", "font_size" : "{conf["font_size"]}" }}'
     client.send(msg)
     time.sleep(1)
 
     # Camera config
     # set any field to Zero to get the default camera setting.
-    # this will position the camera right above the car, with max fisheye and wide fov
-    # this also changes the img output to 255x255x1 ( actually 255x255x3 just all three channels have same value)
     # the offset_x moves camera left/right
     # the offset_y moves camera up/down
     # the offset_z moves camera forward/back
     # with fish_eye_x/y == 0.0 then you get no distortion
     # img_enc can be one of JPG|PNG|TGA
-    # msg = '{ "msg_type" : "cam_config", "fov" : "170", "fish_eye_x" : "1.0", "fish_eye_y" : "1.0", "img_w" : "255", "img_h" : "255", "img_d" : "3", "img_enc" : "JPG", "offset_x" : "0.0", "offset_y" : "3.0", "offset_z" : "0.0", "rot_x" : "90.0" }'
-    # msg = '{ "msg_type" : "cam_config", "fov" : "90", "fish_eye_x" : "0.0", "fish_eye_y" : "0.0", "img_w" : "160", "img_h" : "120", "img_d" : "3", "img_enc" : "PNG", "offset_x" : "0.0", "offset_y" : "1.0", "offset_z" : "1.0", "rot_x" : "15.0" }'
-    msg = '{ "msg_type" : "cam_config", "fov" : "90", "fish_eye_x" : "0.0", "fish_eye_y" : "0.0", "img_w" : "251", "img_h" : "141", "img_d" : "3", "img_enc" : "PNG", "offset_x" : "0.0", "offset_y" : "4.0", "offset_z" : "1.0", "rot_x" : "0.0" }'
+    msg = '{ "msg_type" : "cam_config", "fov" : "165", "fish_eye_x" : "0.0", "fish_eye_y" : "0.0", "img_w" : "512", "img_h" : "288", "img_d" : "3", "img_enc" : "PNG", "offset_x" : "0.0", "offset_y" : "0.0", "offset_z" : "0.0", "rot_x" : "-68.0" }'
     client.send(msg)
     time.sleep(1)
-
-
-    # Controller setup
-    # events = [EV_ABS.ABS_X, EV_ABS.ABS_RZ, EV_ABS.ABS_Z] 
-
-    # adjust steering dead zone. clunky
-    # ctr.dev.absinfo[EV_ABS.ABS_X] = InputAbsInfo(flat=3000)
 
     # Drive car
     do_drive = True
@@ -135,11 +105,10 @@ def drive():
         try:
             client.update()
             if client.aborted:
-                print("Client socket problem, stopping driving.")
+                print("Client aborted, stopping driving.")
                 do_drive = False
         except KeyboardInterrupt:
             do_drive = False
-
 
     time.sleep(1.0)
 
@@ -156,7 +125,50 @@ def drive():
     print("client stopped")
 
 
-
 if __name__ == "__main__":
-    drive()
 
+    # Initialize the donkey environment
+    # where env_name one of:
+    env_list = [
+        "generated_road", 
+        "warehouse", 
+        "sparkfun_avc", 
+        "generated_track", 
+        "roboracingleague_1", 
+        "waveshare", 
+        "mini_monaco", 
+        "warren", 
+        "circuit_launch"
+        ]
+
+    parser = argparse.ArgumentParser(description="garnt_client")
+    parser.add_argument(
+        "--sim",
+        type=str,
+        default="sim_path",
+        help="path to unity simulator. maybe be left at default if you would like to start the sim on your own.",
+    )
+    parser.add_argument("--host", type=str, default="127.0.0.1", help="host to use for tcp")
+    parser.add_argument("--port", type=int, default=9091, help="port to use for tcp")
+    parser.add_argument(
+        "--env_name", type=str, default="circuit_launch", help="name of donkey sim environment", choices=env_list
+    )
+
+    args = parser.parse_args()
+
+    conf = {
+        "exe_path": args.sim,
+        "host": args.host,
+        "port": args.port,
+        "body_style": "car01", # donkey, bare, car01, cybertruck, f1
+        "body_rgb": (234, 21, 144),
+        "car_name": "",
+        "font_size": 10,
+        "racer_name": "Grant",
+        "country": "California",
+        "bio": "custom client",
+        "guid": str(uuid.uuid4()),
+        "start_delay": 1,
+    }
+
+    run_client(args.env_name, conf)
