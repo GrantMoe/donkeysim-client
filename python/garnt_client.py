856cf01ef03daefe11083d8150037dcf7c17ca3c
grantmoe <grant@grantmoe.com>
2021-11-05 12:08:10 -0700

    Change camera data to greyscale w/o fisheye distortion
    Previously, no cam_config message was sent, resulting in default values.
    
    Now, cam_config is sent with all default values except for:
        "fish_eye_x" : "0.0",
        "fish_eye_y" : "0.0",
        "img_d" : "1",
    
    This removes fisheye camera distortion changes images to single-channel
    instead of the default 3.

--- python/garnt_client.py
+++ python/garnt_client.py
@@ -228,9 +228,9 @@ def run_client(env_name, conf):
     # the offset_z moves camera forward/back
     # with fish_eye_x/y == 0.0 then you get no distortion
     # img_enc can be one of JPG|PNG|TGA
-    # msg = '{ "msg_type" : "cam_config", "fov" : "90", "fish_eye_x" : "0.0", "fish_eye_y" : "0.0", "img_w" : "1280", "img_h" : "960", "img_d" : "1", "img_enc" : "PNG", "offset_x" : "0.0", "offset_y" : "0.0", "offset_z" : "0.0", "rot_x" : "0.0" }'
-    # client.send(msg)
-    # time.sleep(1)
+    msg = '{ "msg_type" : "cam_config", "fov" : "0", "fish_eye_x" : "0.0", "fish_eye_y" : "0.0", "img_w" : "0", "img_h" : "0", "img_d" : "1", "img_enc" : "PNG", "offset_x" : "0", "offset_y" : "0", "offset_z" : "0", "rot_x" : "0" }'
+    client.send(msg)
+    time.sleep(1)
 
     # Drive car
     do_drive = True
