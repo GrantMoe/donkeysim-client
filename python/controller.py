93fef4a4a670e08dca40516f556c03fd8dd54ef5
grantmoe <grant@grantmoe.com>
2021-11-05 20:43:23 -0700

    Added explicit reference to supress warnings

--- python/controller.py
+++ python/controller.py
@@ -8,27 +8,27 @@ from libevdev import *
 ev_map = {
     'xbox': {
         'axis': {
-            'left_stick_horz': EV_ABS.ABS_X,
-            'left_stick_vert': EV_ABS.ABS_Y,
-            'left_trigger': EV_ABS.ABS_Z,
-            'right_stick_horz': EV_ABS.ABS_RX,
-            'right_stick_vert': EV_ABS.ABS_RY,
-            'right_trigger': EV_ABS.ABS_RZ,
-            'dpad_horz': EV_ABS.ABS_HAT0X,
-            'dpad_vert': EV_ABS.ABS_HAT0Y,
+            'left_stick_horz': libevdev.EV_ABS.ABS_X,
+            'left_stick_vert': libevdev.EV_ABS.ABS_Y,
+            'left_trigger':libevdev.EV_ABS.ABS_Z,
+            'right_stick_horz': libevdev.EV_ABS.ABS_RX,
+            'right_stick_vert': libevdev.EV_ABS.ABS_RY,
+            'right_trigger': libevdev.EV_ABS.ABS_RZ,
+            'dpad_horz': libevdev.EV_ABS.ABS_HAT0X,
+            'dpad_vert': libevdev.EV_ABS.ABS_HAT0Y,
         },
         'button': {
-            'a_button': EV_KEY.BTN_SOUTH,
-            'b_button': EV_KEY.BTN_EAST,
-            'y_button': EV_KEY.BTN_NORTH,
-            'x_button': EV_KEY.BTN_WEST,
-            'left_shoulder': EV_KEY.BTN_TL,
-            'right_shoulder': EV_KEY.BTN_TR,
-            'select_button': EV_KEY.BTN_SELECT,
-            'start_button': EV_KEY.BTN_START,
-            'mode_button': EV_KEY.BTN_MODE,
-            'left_thumb': EV_KEY.BTN_THUMBL,
-            'right_thumb': EV_KEY.BTN_THUMBR,
+            'a_button': libevdev.EV_KEY.BTN_SOUTH,
+            'b_button': libevdev.EV_KEY.BTN_EAST,
+            'y_button': libevdev.EV_KEY.BTN_NORTH,
+            'x_button': libevdev.EV_KEY.BTN_WEST,
+            'left_shoulder': libevdev.EV_KEY.BTN_TL,
+            'right_shoulder': libevdev.EV_KEY.BTN_TR,
+            'select_button': libevdev.EV_KEY.BTN_SELECT,
+            'start_button': libevdev.EV_KEY.BTN_START,
+            'mode_button': libevdev.EV_KEY.BTN_MODE,
+            'left_thumb': libevdev.EV_KEY.BTN_THUMBL,
+            'right_thumb': libevdev.EV_KEY.BTN_THUMBR,
         }
     }
 }
@@ -49,29 +49,29 @@ class Controller:
                 raise e
 
         self.map_ = ev_map[ctr_type]
-        self.dev_.absinfo[EV_ABS.ABS_X] = InputAbsInfo(flat=3000)
+        self.dev_.absinfo[libevdev.EV_ABS.ABS_X] = InputAbsInfo(flat=3000)
 
 
     def steering_deadzone(self, val):
         if val is None:
-            return self.dev_.absinfo[EV_ABS.ABS_X].flat
+            return self.dev_.absinfo[libevdev.EV_ABS.ABS_X].flat
         else:
             ai = InputAbsInfo(flat=val)
-            self.dev_.absinfo[EV_ABS.ABS_X] = ai
+            self.dev_.absinfo[libevdev.EV_ABS.ABS_X] = ai
 
     # see if this counts as enough work to actually refresh
     def update(self):
         try:
             for event in self.dev_.events():
-                if event.matches(EV_ABS):
+                if event.matches(libevdev.EV_ABS):
                     continue
-                elif event.matches(EV_KEY):
+                elif event.matches(libevdev.EV_KEY):
                     continue
         except EventsDroppedException:
             for sync_event in self.dev_.sync():
-                if sync_event.matches(EV_ABS):
+                if sync_event.matches(libevdev.EV_ABS):
                     continue
-                if sync_event.matches(EV_KEY):
+                if sync_event.matches(libevdev.EV_KEY):
                     continue
         except:
             raise Exception('polling failure')
