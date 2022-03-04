3fbe60cbae06dfa403606e9cc988a26786812bb2
grantmoe <grant@grantmoe.com>
2022-03-04 11:16:14 -0800

    Clean/tweak autopilot
    Add some comment
    
    Modify lstm processing method to take advantage of maxlen to avoid
    having to pop from the queue before appending, since that will be taken
    care of automatically.

--- python/autopilot.py
+++ python/autopilot.py
@@ -15,33 +15,42 @@ class Autopilot:
         self.model_number = conf['model_number']
         data = model_paths(conf['model_history'], conf['model_number'])
         self.model = load_model(f"{MODEL_DIRECTORY}/{data['model_file']}", compile=False)
-        # Playing with fire here a bit
+        ## try to detect if prior image norming is required (currently broken)
         # print(self.model.get_layer(index=1).name)
         # self.norm_images = self.model.get_layer(index=1).name != 'rescaling'
         self.scaler = pickle.load(open(f"{SCALER_DIRECTORY}/{data['scaler_file']}", 'rb'))
         self.telemetry_columns = data['telemetry_columns']
         self.model_type = conf['model_type']
         if self.model_type == 'lstm':
-            self.img_seq = deque()
-            self.tel_seq = deque()
             self.sequence_length = conf['sequence_length']
+            # using maxlen will take care of 'popping'
+            self.img_seq = deque(maxlen=self.sequence_length)
+            self.tel_seq = deque(maxlen=self.sequence_length)
 
+        print('autopilot initiated')
+
+    # converts from 0-255 uint to expected 0.0-1.0 float
     def norm_image(self, img):
         img_array = img_to_array(img)
         return img_array / 255
 
+    # load up sequence queues and shape accordingly
     def process_lstm(self, img, tel):
 
+        # if the queues are empty, fill them with first image
         while len(self.img_seq) < self.sequence_length:
             self.img_seq.append(img)
+        while len(self.tel_seq) < self.sequence_length:
             self.tel_seq.append(tel)
         
-        self.img_seq.popleft()
-        self.tel_seq.popleft()
+         # add newest input
         self.img_seq.append(img)
         self.tel_seq.append(tel)
+        
+        # shape as expected by model
         img_array = np.array(self.img_seq).reshape(-2, 3, 120, 160, 1)
-        tel_array = np.array(self.tel_seq).reshape(-1, 3, 5, 1) #.resize((3, 5, 1))
+        tel_array = np.array(self.tel_seq).reshape(-1, 3, 5, 1)
+        
         return img_array, tel_array
 
     def infer(self, inputs):
@@ -50,20 +59,19 @@ class Autopilot:
             img = self.norm_image(inputs[0])
             imu = self.scaler.transform(np.array([inputs[1]]))
             img_in, imu_in = self.process_lstm(img, imu)
-            # print(imu_in)
-
-        else:
+        else: # self.model_ty[e == 'vimu':
             if self.model_number < 309:
                 img = self.norm_image(inputs[0])
             else:
                 img = img_to_array(inputs[0], dtype='uint8')
-                
             imu = np.array([inputs[1]])
             imu_in = self.scaler.transform(imu)
             img_in = img.reshape((1,)+img.shape)
 
+        # grab inference
         pred = self.model([img_in, imu_in], training=False)
         
+        # checks if single or dual ouput based on return type
         if isinstance(pred, list):
             st_pred = pred[0].numpy()[0][0]
             th_pred = pred[1].numpy()[0][0]
