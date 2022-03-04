183f89262d7d707ec07a6041cb0e4215e3fc1be5
grantmoe <grant@grantmoe.com>
2022-03-03 17:43:37 -0800

    Fix telemetry input shape error

--- python/autopilot.py
+++ python/autopilot.py
@@ -17,44 +17,53 @@ class Autopilot:
         self.model = load_model(f"{MODEL_DIRECTORY}/{data['model_file']}", compile=False)
         # Playing with fire here a bit
         # print(self.model.get_layer(index=1).name)
-        # self.convert_images = self.model.get_layer(index=1).name != 'rescaling'
+        # self.norm_images = self.model.get_layer(index=1).name != 'rescaling'
         self.scaler = pickle.load(open(f"{SCALER_DIRECTORY}/{data['scaler_file']}", 'rb'))
         self.telemetry_columns = data['telemetry_columns']
         self.model_type = conf['model_type']
         if self.model_type == 'lstm':
-            self.img_queue = deque()
-            self.tel_queue = deque()
+            self.img_seq = deque()
+            self.tel_seq = deque()
             self.sequence_length = conf['sequence_length']
 
-    def convert_image(self, img):
+    def norm_image(self, img):
         img_array = img_to_array(img)
         return img_array / 255
 
     def process_lstm(self, img, tel):
-        while len(self.img_queue) < self.sequence_length:
-            self.img_queue.append(img)
-            self.tel_queue.append(tel)
-        self.img_queue.popleft()
-        self.tel_queue.popleft()
-        self.img_queue.append(img)
-        self.tel_queue.append(tel)
-        return self.img_queue, self.tel_queue
+
+        while len(self.img_seq) < self.sequence_length:
+            self.img_seq.append(img)
+            self.tel_seq.append(tel)
+        
+        self.img_seq.popleft()
+        self.tel_seq.popleft()
+        self.img_seq.append(img)
+        self.tel_seq.append(tel)
+        img_array = np.array(self.img_seq).reshape(-2, 3, 120, 160, 1)
+        tel_array = np.array(self.tel_seq).reshape(-1, 3, 5, 1) #.resize((3, 5, 1))
+        return img_array, tel_array
 
     def infer(self, inputs):
-        # for i in range(len(inputs[1])):
-        #     if type(inputs[1][i]) is int:
-        #         inputs[1][i] = inputs[1][i].astype(uint8)
-            # print(f'type(inputs[1][{i}]) = {inputs[1][i].dtype}')
-        if self.model_number < 309:
-            img = self.convert_image(inputs[0])
-        else:
-            img = img_to_array(inputs[0])
-        imu = np.array([inputs[1]])
-        imu_in = self.scaler.transform(imu)
-        img_in = img.reshape((1,)+img.shape)
+        
         if self.model_type == 'lstm':
-            img_in, imu_in = self.process_lstm(img_in, imu_in)
+            img = self.norm_image(inputs[0])
+            imu = self.scaler.transform(np.array([inputs[1]]))
+            img_in, imu_in = self.process_lstm(img, imu)
+            # print(imu_in)
+
+        else:
+            if self.model_number < 309:
+                img = self.norm_image(inputs[0])
+            else:
+                img = img_to_array(inputs[0], dtype='uint8')
+                
+            imu = np.array([inputs[1]])
+            imu_in = self.scaler.transform(imu)
+            img_in = img.reshape((1,)+img.shape)
+
         pred = self.model([img_in, imu_in], training=False)
+        
         if isinstance(pred, list):
             st_pred = pred[0].numpy()[0][0]
             th_pred = pred[1].numpy()[0][0]
